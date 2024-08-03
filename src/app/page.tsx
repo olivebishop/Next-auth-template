0949f3ede37df23ef17ee9d6c0d91ba7d58aefc2
olivebishop <olivehendrilgen1@gmail.com>
2024-08-03 05:00:41 +0300

    fix: page main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import react from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+     <h1 className="text-4xl font-bold">Lucia auth</h1>
     </main>
   );
 }
