09ce7d36a41e317fb8c5ab6962e0689ea2c14605
olivebishop <olivehendrilgen1@gmail.com>
2024-08-05 01:23:30 +0300

    fix: done chnages on linters and prisma config

--- src/app/api/auth/google/login/route.ts
+++
@@ -1,8 +0,0 @@
-import { googleAuth } from "@/lib/auth";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const [url, state] = await googleAuth.getAuthorizationUrl();
-  // Store state (you might want to use cookies or some other method to store the state)
-  return NextResponse.redirect(url.toString());
-}
